1365b55b44b5c11fbc8d0ad198d47b9d9f9b1ecf
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-03-01 16:00:58 -0300

    check the time when creating appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -9,9 +10,8 @@ class AppointmentController {
       date: Yup.date().required()
     });
 
-    if (!(await schema.isValid(req.body))) {
+    if (!(await schema.isValid(req.body)))
       return res.status(400).json({ error: 'Bad request' });
-    }
 
     const { provider_id, date } = req.body;
 
@@ -19,20 +19,45 @@ class AppointmentController {
      * Check if provider_id is a provider
      */
 
-    const isProvider = await User.findOne({
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true }
     });
 
-    if (!isProvider) {
+    if (!checkIsProvider) {
       return res
         .status(401)
-        .json({ error: 'You can create appointments with providers' });
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date()))
+      return res.status(400).json({ error: 'Past dates are not permited' });
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date
+      date: hourStart
     });
 
     return res.json(appointment);
