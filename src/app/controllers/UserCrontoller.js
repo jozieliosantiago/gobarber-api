5076c6b47901b177f64527470a832acf110209c7
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-23 22:23:16 -0300

    validation of the avatar attribute included in the schema

--- src/app/controllers/UserCrontoller.js
+++ src/app/controllers/UserCrontoller.js
@@ -37,6 +37,7 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
+      avatar: Yup.number(),
       oldPassword: Yup.string().min(6),
       password: Yup.string()
         .min(6)
